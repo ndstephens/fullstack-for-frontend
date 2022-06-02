5a09c4feb478ce036193e5e343c885801464e1a2
FSFFE Digital Ocean <nate.dev@fivefloral.com>
2022-06-02 16:00:40 +0000

    updated app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/test', (req, res) => {
+	res.status(418).set('X-Test-Header', 'YO').send('test response message');
+});
+
 app.get('/', (req, res) => {
 	res.send('My Node App');
 });
